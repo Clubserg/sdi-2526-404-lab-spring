9784c5aafb3d37f8d825ea82671ff6413b26cfee
Clubserg <gdclubserg@gmail.com>
2026-02-10 20:44:07 +0100

    SDI-2526-404-2.1-Creating Spring Project.

---
+++ src/main/java/com/example/grademanager/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.example.grademanager;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(GradeManagerApplication.class);
+    }
+
+}
