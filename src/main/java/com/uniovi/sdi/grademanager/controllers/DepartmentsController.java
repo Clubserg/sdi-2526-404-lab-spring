e4ea0242d78500e60653f9c558f2c888ed305ae7
uo302282 <gdclubserg@gmail.com>
2026-02-11 13:54:01 +0100

    SDI-2526-404-sesión3-ejercicio-complementario2

--- src/main/java/com/uniovi/sdi/grademanager/controllers/DepartmentsController.java
+++ src/main/java/com/uniovi/sdi/grademanager/controllers/DepartmentsController.java
@@ -3,52 +3,57 @@ package com.uniovi.sdi.grademanager.controllers;
 import com.uniovi.sdi.grademanager.entities.Department;
 import com.uniovi.sdi.grademanager.services.DepartmentsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+@Controller
 public class DepartmentsController {
 
     @Autowired
     private DepartmentsService departmentsService;
 
     @GetMapping("/departments")
-    public String getDepartments() {
-        return departmentsService.getDepartmentList().toString();
+    public String getDepartments(Model model) {
+        model.addAttribute("departmentList", departmentsService.getDepartmentList());
+        return "department/list";
     }
 
     @GetMapping("/departments/details/{id}")
-    public String getDepartmentDetails(@PathVariable Long id) {
-        return departmentsService.getDepartment(id).toString();
+    public String getDepartmentDetails(Model model, @PathVariable Long id) {
+        model.addAttribute("department", departmentsService.getDepartment(id));
+        return "department/details";
     }
 
     @GetMapping("/departments/delete/{id}")
     public String deleteDepartment(@PathVariable Long id) {
         departmentsService.deleteDepartment(id);
-        return "eliminando departamento" + id;
+        return "redirect:/departments";
     }
 
     @GetMapping("/departments/add")
-    public String addDepartment() {
-
-        return "añadir departamento";
+    public String addDepartment(Model model) {
+        model.addAttribute("department", new Department());
+        return "department/add";
     }
 
     @PostMapping(value = "/departments/add")
     public String setDepartment(@ModelAttribute Department department) {
         departmentsService.addDepartment(department);
-        return "añadiendo departamento";
+        return "redirect:/departments";
     }
 
-    @RequestMapping(value = "/departments/edit/{id}")
-    public String getEdit(@PathVariable Long id) {
-        return "editando departamento " + id;
+    @GetMapping("/departments/edit/{id}")
+    public String getEdit(Model model, @PathVariable Long id) {
+        model.addAttribute("department", departmentsService.getDepartment(id));
+        return "department/edit";
     }
 
-    @PostMapping("/departments/edit")
-    public String setEdit(@ModelAttribute Department department) {
+    // Procesar la edición
+    @PostMapping("/departments/edit/{id}")
+    public String setEdit(@PathVariable Long id, @ModelAttribute Department department) {
+        department.setId(id);
         departmentsService.addDepartment(department);
-        return "editando departamento";
+        return "redirect:/departments";
     }
-
-
 }
